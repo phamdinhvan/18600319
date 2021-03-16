12cf05d101554d8ae9f1a7bc57cde4b2a4a55c49
Your Name <phamvanhcmus>
2021-03-16 20:43:57 +0700

    update- port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 console.log (`Serve is listen on port ${port}`);
 app.listen(port);
